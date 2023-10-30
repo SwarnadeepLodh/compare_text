6b184b7435931428148b4e384abfe0ec89177b72
SwarnadeepLodh <76892690+SwarnadeepLodh@users.noreply.github.com>
2023-10-30 18:27:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ async def compare_texts(texts: Texts = Body(...)):
 
 # Running the Application
 if __name__ == '__main__':
-    uvicorn.run("compare_text:app",reload=True)
+    uvicorn.run("main:app",reload=True)
